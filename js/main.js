975552b8e6787348a73a5cbe79c032ca8b1397a0
DownTheMatrix <suija87@gmail.com>
2019-07-01 14:41:00 +0200

    Wrap code into initialize function

--- js/main.js
+++ js/main.js
@@ -1,25 +1,29 @@
-/* Navigation */
+/* Initialize function */
+function init() {} /* Navigation */
 const TOGGLE = document.querySelector(".toggler");
 const NAV_LIST = document.querySelector(".navigation-list");
 
+// Toggle the hamburger icons on and off
 TOGGLE.addEventListener("click", function() {
-    NAV_LIST.classList.toggle("active");
-})
+  NAV_LIST.classList.toggle("active");
+});
 
 /* Statistics / Dashboard */
 const DASHBOARD_WRAPPER = document.querySelector(".dashboard-wrapper");
 const DASHBOARD_IMG = document.querySelector(".dashboard-img");
-
 const STATS_ICON = document.querySelectorAll(".statistics-icon");
 
+// Iterate over the icons in the statistics section and add a click event to each of them
 for (let i = 0; i < STATS_ICON.length; i++) {
-    STATS_ICON[i].addEventListener("click", function(evt) {
-       if(STATS_ICON[i].classList.contains("custom-dashboard")) {
-           DASHBOARD_IMG.src = "./img/dashboard-light.jpg";
-       } else if (STATS_ICON[i].classList.contains("smart-analytics")) {
-            DASHBOARD_IMG.src = "./img/smart-analytics.jpg";
-       } else {
-            DASHBOARD_IMG.src = "./img/dashboard-dark.jpg";
-       }
-    });
+  STATS_ICON[i].addEventListener("click", function(evt) {
+    if (STATS_ICON[i].classList.contains("app-ui-img")) {
+      DASHBOARD_IMG.src = "./img/movie-app-ui.png";
+    } else if (STATS_ICON[i].classList.contains("landscape-img")) {
+      DASHBOARD_IMG.src = "./img/valley-landscape.png";
+    } else {
+      DASHBOARD_IMG.src = "./img/watersports-icons.png";
+    }
+  });
 }
+
+document.addEventListener("DOMContentLoaded", init);
